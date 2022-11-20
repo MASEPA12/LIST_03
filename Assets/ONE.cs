aa7c4630db9b03441717b97260f8219fb048c9f0
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-20 10:38:39 +0100

    exercise1???

---
+++ Assets/ONE.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ONE : MonoBehaviour
+{
+    public string letter;
+
+    void Start()
+    {
+        if(isVowel = true)
+        {
+            Debug.Log
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool isVowel()
+    {
+        if(letter == a || letter == e || letter == i || letter == o || letter == u)
+        {
+            isVowel = true
+        }
+    }
+}
