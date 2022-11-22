7a5f97285cc678b1613302954643e090cf74f77c
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-22 08:06:33 +0100

    exercise5

--- Assets/FIVE.cs
+++ Assets/FIVE.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FIVE : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int Number;
+
     void Start()
     {
-        
+        MultuplicatonTable(Number, 1);
+        MultuplicatonTable(Number, 2);
+        MultuplicatonTable(Number, 3);
+        MultuplicatonTable(Number, 4);
+        MultuplicatonTable(Number, 5);
+        MultuplicatonTable(Number, 6);
+        MultuplicatonTable(Number, 7);
+        MultuplicatonTable(Number, 8);
+        MultuplicatonTable(Number, 9);
+        MultuplicatonTable(Number, 10);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MultuplicatonTable(int n, int N)
     {
-        
+        int result1 = n * N;
+        Debug.Log($"{n} x {N} = {result1}");
     }
 }
