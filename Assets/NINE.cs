75d8f69e8b7770cca8261c9f9bf4ab6e742b2ba0
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-22 17:52:51 +0100

    nine

--- Assets/NINE.cs
+++ Assets/NINE.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class NINE : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float b;
+    public float h;
+
     void Start()
     {
-        
+        areaCalculator(b, h);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void areaCalculator(float B,float H)
     {
-        
+        float result = (B * H)/2;
+        if (B > 0 && H > 0) //if all are not 0 or negative the area canbe calculated
+        {
+            Debug.Log($"The area of the triangle witch base is {B} and height {H} is {result}.");
+        }
+        else //if B or H is equal to 0 or negative, it cannot be calculated
+        {
+            Debug.Log($"The area cannot be calculated.");
+        }
+
     }
 }
