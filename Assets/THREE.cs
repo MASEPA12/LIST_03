037b24f21c5f757146b3ab0185cc20fea819e021
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-12-13 17:55:25 +0100

    DONE

--- Assets/THREE.cs
+++ Assets/THREE.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class THREE : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public int dividend; 
+    public int divisor; 
         
+    void Start()
+    {   //if one of the numbers is 0, we cannot divide 
+        if (dividend == 0 || divisor == 0)
+        {
+            Debug.Log("The division cannot be made");
+        }
+        else //but if the're not, we call the function
+        {
+            isExact(dividend, divisor);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void isExact(int D, int d)
     {
-        
+        int result = D % d;
+        if (result == 0)
+        {
+            Debug.Log($"The division between {D} and {divisor} is exact");
+        }
+        else
+        {
+            Debug.Log($"The division between {D} and {d} is not exact");
+        }
     }
+
 }
--- Assets/TWO.cs
+++ Assets/TWO.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class TWO : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int number;
     void Start()
-    {
-        
+    { //if the function is true or false...
+        if(isEven(number) == true)
+        {
+            Debug.Log($"The number {number} is even.");
+        }
+        else
+        {
+            Debug.Log($"The number {number} is NOT even.");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private bool isEven(int N)
+    { //if the number can be divided by 2 (that it's quotient is 0) it will be even
+        int result = N % 2;
+        if(result == 0)
+        {
+            return true;
+        }
+        return false;
     }
 }
